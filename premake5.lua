ee71030e390c8865a37fb4d09f7cf08ee04c885c
holychicken99 <akshit.singh20@gmail.com>
2022-01-10 19:27:14 -0800

    added premake config

---
+++ premake5.lua
@@ -0,0 +1,18 @@
+ workspace " ChatApp"  
+configurations { "Debug", "Release" } 
+
+project "ChatApp"  
+kind "ConsoleApp"   
+language "C++"   
+
+targetdir "build/%{cfg.buildcfg}" 
+files { " ./server/**.cpp" } 
+includedirs { "./includes/**",  "~/Documents/Headers/boost/"}
+buildoptions {"-Wall","-g"}
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }  
+        symbols "On" 
+    filter "configurations:Release"  
+        defines { "NDEBUG" }    
+        optimize "On" 
